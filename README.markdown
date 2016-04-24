de71f2a927ec62b8857bd4fbc905c69b7b8d22fc
NOBUOKA Yu <nobuoka@vividcode.info>
2016-04-25 01:00:47 +0900

    Create project (by using Android Studio 2.0)

---
+++ README.markdown
@@ -0,0 +1,4 @@
+Jenkins Pipeline Sample for Android app
+==================================================
+
+Jenkins Pipeline でジョブを実行する Android プロジェクトのサンプルです。
---
+++ app/src/test/java/info/vividcode/android/app/samples/jenkinspipeline/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package info.vividcode.android.app.samples.jenkinspipeline;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
